e9d3ec49ba57e44105788c240eb5b38fba618338
kirillsankov <73188231+kirillsankov@users.noreply.github.com>
2022-12-06 09:20:33 +0300

    dsds

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 
 
 console.log("Hig");
+
+alert("dsdada");
+alert("dsdada");
+alert("dsdada");
+alert("ds");
+alert("dsdada");
+alert("dsdada");
+alert("dsdada");
+alert("dsdada");
+alert("dsdada");
+alert("dsdada");
